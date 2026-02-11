9bafea056faad03389dc1fb49130b36a445c338b
Muhammad  Rafsanjani Alfarisi <rafsanjanialfarisi21@gmail.com>
2026-02-11 11:04:19 +0700

    Menambahkan halaman sejarah singkat di home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
     @include('partials.sections.home.hero')
+    @include('partials.sections.home.sejarah')
     @include('partials.sections.home.tentang')
     @include('partials.sections.home.program')
     @include('partials.sections.home.berita-prestasi-testi')
