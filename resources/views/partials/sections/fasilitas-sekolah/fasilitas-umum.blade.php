f40430a4f0ca5383a15da7cd465653ac9475dae5
muhammadfaizramadha <mfaizschl@gmail.com>
2026-04-03 02:40:15 +0700

    fix Multi Language on fasilitas page

--- resources/views/partials/sections/fasilitas-sekolah/fasilitas-umum.blade.php
+++ resources/views/partials/sections/fasilitas-sekolah/fasilitas-umum.blade.php
@@ -19,7 +19,7 @@
                 </div>
             </div>
             @empty
-            <div class="col-span-full text-center py-8 text-gray-400">Belum ada data.</div>
+            <div class="col-span-full text-center py-8 text-gray-400">{{ __('Belum ada data.') }}</div>
             @endforelse
         </div>
     </div>
