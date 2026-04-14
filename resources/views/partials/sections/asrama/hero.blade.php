541ae1a7ecba5f1a9bd2110c3cfddec80dd77cab
Muhammad  Rafsanjani Alfarisi <rafsanjanialfarisi21@gmail.com>
2026-04-14 11:30:23 +0700

    Update Hero Section Asrama

--- resources/views/partials/sections/asrama/hero.blade.php
+++ resources/views/partials/sections/asrama/hero.blade.php
@@ -1,5 +1,6 @@
-<section class="relative w-full h-80 md:h-100 lg:h-120 overflow-hidden mb-4">
-    <img src="{{ asset('assets/Asrama.jpg') }}" alt="asrama" class="absolute inset-0 w-full h-210 object-cover">
+<section class="relative w-full h-64 md:h-100 lg:h-120 overflow-hidden mb-4">
+    <img src="{{ asset('assets/Hero_Section_Asrama.webp') }}?v={{ filemtime(public_path('assets/Hero_Section_Asrama.webp')) }}"
+        alt="asrama" class="absolute inset-0 w-full h-full object-cover">
 
     <!-- Overlay -->
     <div class="absolute inset-0 bg-black/45"></div>
